1564b9bff4d0b77bb23f7539268e407644ee606d
Taufik Hidayatullah <taufikhh.97@gmail.com>
2025-01-19 02:28:17 +0700

    db migrations

---
+++ migrations/1737224626091_first-migration.js
@@ -0,0 +1,30 @@
+exports.up = (pgm) => {
+  pgm.createTable('albums', {
+    id: { type: 'VARCHAR(25)', primaryKey: true }, //"album-Mk8AnmCp210PwT6B", album-16digitrandom
+    name: { type: 'TEXT', notNull: true },
+    year: { type: 'integer', notNull: true },
+    created_at: { type: 'TEXT', notNull: true },
+    updated_at: { type: 'TEXT', notNull: true },
+  });
+
+  pgm.createTable('songs', {
+    id: { type: 'VARCHAR(25)', primaryKey: true }, //"song-Qbax5Oy7L8WKf74l", song-16digitrandom
+    albumId: {
+      type: 'VARCHAR(25)',
+      references: 'albums(id)',
+      onDelete: 'CASCADE',
+    }, //"album-Mk8AnmCp210PwT6B", album-16digitrandom
+    title: { type: 'TEXT', notNull: true },
+    year: { type: 'integer', notNull: true },
+    performer: { type: 'TEXT', notNull: true },
+    genre: { type: 'TEXT', notNull: true },
+    duration: { type: 'integer' },
+    created_at: { type: 'TEXT', notNull: true },
+    updated_at: { type: 'TEXT', notNull: true },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('songs');
+  pgm.dropTable('albums');
+};
