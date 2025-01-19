70ca97ba37cf7a0f2bce89d2a1782364ec28ce6a
Taufik Hidayatullah <taufikhh.97@gmail.com>
2025-01-19 17:59:55 +0700

    update file migrations

---
+++ migrations/1737284186519_create-table-albums.js
@@ -0,0 +1,13 @@
+exports.up = (pgm) => {
+  pgm.createTable('albums', {
+    id: { type: 'VARCHAR(25)', primaryKey: true }, //"album-Mk8AnmCp210PwT6B", album-16digitrandom
+    name: { type: 'TEXT', notNull: true },
+    year: { type: 'integer', notNull: true },
+    created_at: { type: 'TEXT', notNull: true },
+    updated_at: { type: 'TEXT', notNull: true },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('albums');
+};
--- migrations/1737284234297_create-table-songs.js
+++ migrations/1737284234297_create-table-songs.js
@@ -1,12 +1,4 @@
 exports.up = (pgm) => {
-  pgm.createTable('albums', {
-    id: { type: 'VARCHAR(25)', primaryKey: true }, //"album-Mk8AnmCp210PwT6B", album-16digitrandom
-    name: { type: 'TEXT', notNull: true },
-    year: { type: 'integer', notNull: true },
-    created_at: { type: 'TEXT', notNull: true },
-    updated_at: { type: 'TEXT', notNull: true },
-  });
-
   pgm.createTable('songs', {
     id: { type: 'VARCHAR(25)', primaryKey: true }, //"song-Qbax5Oy7L8WKf74l", song-16digitrandom
     album_id: {
@@ -26,5 +18,4 @@ exports.up = (pgm) => {
 
 exports.down = (pgm) => {
   pgm.dropTable('songs');
-  pgm.dropTable('albums');
 };
