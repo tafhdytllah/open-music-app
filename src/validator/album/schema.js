ea42a1292f906e4dacd43b11846d38b323cf5799
Taufik Hidayatullah <taufikhh.97@gmail.com>
2025-02-02 18:08:58 +0700

    update code

--- src/validator/album/schema.js
+++ src/validator/album/schema.js
@@ -1,11 +1,12 @@
 const Joi = require("joi");
 
+const currentYear = new Date().getFullYear();
 /**
  * Schema for Album Payload
  */
 const AlbumPayloadSchema = Joi.object({
   name: Joi.string().required(),
-  year: Joi.number().required(),
+  year: Joi.number().integer().min(1900).max(currentYear).required(),
 });
 
 module.exports = { AlbumPayloadSchema };
